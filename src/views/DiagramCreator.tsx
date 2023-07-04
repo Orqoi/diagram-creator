641606c1fdf61fa4f2d03956b3e00f9706210393
Orqoi <lewklconrad@gmail.com>
2023-07-04 16:39:09 +0800

    Create examples for custom nodes and edges

--- src/views/DiagramCreator.tsx
+++ src/views/DiagramCreator.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@mui/material';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import ReactFlow, {
   Controls,
   useNodesState,
@@ -8,16 +8,23 @@ import ReactFlow, {
 } from 'reactflow';
 
 import NodeDrawer from '../components/NodeDrawer';
-
-const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+import TextUpdaterNode from '../components/nodes/TextUpdaterNode';
+import CustomEdge from '../components/edges/CustomEdge';
 
 function DiagramCreator() {
-const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
+
+    const initialNodes = [
+        { id: '1', type: 'textUpdater', position: { x: 0, y: 0 }, data: { label: '1' } },
+        { id: '2', type: 'textUpdater', position: { x: 0, y: 100 }, data: { label: '2' } },
+    ];
+
+    const edgeTypes = {
+        customedge: CustomEdge,
+    };
+
+    const initialEdges = [{ id: 'e1-2', source: '1', target: '2', type: 'customedge' }];
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
   return (
@@ -25,6 +32,8 @@ const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
         <NodeDrawer />
 
         <ReactFlow
+          nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
